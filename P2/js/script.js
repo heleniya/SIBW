c1bbf1fd5e62100ca9dd9e82ab6b3bc8c1e0e307
Helena <hruizaranda@correo.ugr.es>
2024-03-26 21:55:07 +0100

    el formulario ya esta, falta arreglar correo, censura y que se guarde el comentario

--- P2/js/script.js
+++ P2/js/script.js
@@ -16,3 +16,43 @@ function funcion_mostrar() {
 }
 
 boton.addEventListener('click', funcion_mostrar);
+
+//boostrap
+document.addEventListener('DOMContentLoaded', (event) => {
+    'use strict';
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation');
+  
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault();
+            event.stopPropagation();
+          }
+  
+          form.classList.add('was-validated');
+        }, false);
+      });
+  });
+
+  document.addEventListener('DOMContentLoaded', (event) => {
+    document.getElementById('formId').addEventListener('submit', function(event) {
+        const emailInput = document.getElementById('customEmail');
+        const regex = new RegExp('[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$');
+        if (!regex.test(emailInput.value)) {
+            // Prevenir la entrega del formulario
+            event.preventDefault();
+            // Mostrar feedback invalido
+            emailInput.nextElementSibling.style.display = 'block';
+            emailInput.classList.add('is-invalid');
+        } else {
+            // Ocultar feedback si el email es valido
+            emailInput.nextElementSibling.style.display = 'none';
+            emailInput.classList.remove('is-invalid');
+        }
+    });
+});
+  
