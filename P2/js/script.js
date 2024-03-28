274c13af787019da204f05abcc174b5aadb5c209
Helena <hruizaranda@correo.ugr.es>
2024-03-28 19:45:52 +0100

    falta solo el responsive

--- P2/js/script.js
+++ P2/js/script.js
@@ -1,63 +1,3 @@
-/*
-const boton = document.getElementById("mostrarCom");
-var comentarios = document.getElementById("zonaEscondida");
-
-function funcion_mostrar() {    
-    comentarios.classList.toggle('visible');
-    
-    if (comentarios.classList.contains('visible')) {
-        boton.style.transform = 'translateX(-45.25vw) translateY(-10vh)'; // Mueve el botón hacia la izquierda
-
-        boton.textContent = 'Ocultar comentarios'; // Cambia el texto al mostrar los comentarios
-    } else {
-        boton.style.transform = 'rotate(-90deg)'; // Devuelve el botón a su posición original
-        comentarios.style.marginLeft = '100%'; // Oculta la sección moviéndola fuera de la vista
-        boton.textContent = 'Mostrar comentarios'; // Vuelve al texto original al ocultar los comentarios
-    }
-}
-
-boton.addEventListener('click', funcion_mostrar);
-
-//boostrap
-document.addEventListener('DOMContentLoaded', (event) => {
-    'use strict';
-  
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    const forms = document.querySelectorAll('.needs-validation');
-  
-    // Loop over them and prevent submission
-    Array.prototype.slice.call(forms)
-      .forEach(function (form) {
-        form.addEventListener('submit', function (event) {
-          if (!form.checkValidity()) {
-            event.preventDefault();
-            event.stopPropagation();
-          }
-  
-          form.classList.add('was-validated');
-        }, false);
-      });
-  });
-
-  document.addEventListener('DOMContentLoaded', (event) => {
-    document.getElementById('formId').addEventListener('submit', function(event) {
-        const emailInput = document.getElementById('customEmail');
-        const regex = new RegExp('[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$');
-        if (!regex.test(emailInput.value)) {
-            // Prevenir la entrega del formulario
-            event.preventDefault();
-            // Mostrar feedback invalido
-            emailInput.nextElementSibling.style.display = 'block';
-            emailInput.classList.add('is-invalid');
-        } else {
-            // Ocultar feedback si el email es valido
-            emailInput.nextElementSibling.style.display = 'none';
-            emailInput.classList.remove('is-invalid');
-        }
-    });
-});
-*/
-
 document.addEventListener('DOMContentLoaded', () => {
   'use strict';
 
@@ -117,26 +57,25 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
     // Array con las palabras prohibidas
-    const palabrasProhibidas = ['puta ', 'mierda'];
+    const palabrasProhibidas = ['puta', 'tractor'];
         
-    // Seleccionar el campo de texto del comentario
     const comentarioTexto = document.getElementById('floatingTextarea');
 
     // Función para reemplazar palabras prohibidas por asteriscos
     function censurar(texto) {
         palabrasProhibidas.forEach(palabra => {
-            const regex = new RegExp(palabra); // g: global, i: case insensitive
+            const expresion = new RegExp(palabra);
             const asteriscos = '*'.repeat(palabra.length);
-            texto = texto.replace(regex, asteriscos);
+            texto = texto.replace(expresion, asteriscos);
         });
         return texto;
     }
 
     // Escuchar por cambios en el campo de comentario
-    comentarioTexto.addEventListener('input', () => {
+    comentarioTexto.addEventListener('keyup', () => {
         const textoOriginal = comentarioTexto.value;
-        const textoFiltrado = censurar(textoOriginal);
-        comentarioTexto.value = textoFiltrado;
+        const textoCensurado = censurar(textoOriginal);
+        comentarioTexto.value = textoCensurado;
     });
 });
 
